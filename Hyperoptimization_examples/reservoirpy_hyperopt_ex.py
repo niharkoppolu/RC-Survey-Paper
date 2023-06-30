dcd8a5b1d777692f5a86dad5bfa4cbbb897e7bac
Nihar Koppolu <koppolu@pdx.edu>
2023-06-30 13:25:58 -0700

    6/30/23

---
+++ Hyperoptimization_examples/reservoirpy_hyperopt_ex.py
@@ -0,0 +1,131 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import reservoirpy as rpy
+
+rpy.verbosity(0)
+
+from reservoirpy.nodes import Reservoir, Ridge
+from reservoirpy.datasets import mackey_glass
+
+print("1. Reading in mackey glass timeseries")
+
+X = mackey_glass(2000)
+
+
+#Prints mackeyglass timeseries graph
+"""
+plt.figure()
+plt.xlabel("$t$")
+plt.title("Mackey-Glass timeseries")
+plt.plot(X[:500])
+plt.show()
+"""
+
+print("2. defining the objective")
+
+from reservoirpy.observables import nrmse, rsquare
+
+# Objective functions accepted by ReservoirPy must respect some conventions:
+#  - dataset and config arguments are mandatory, like the empty '*' expression.
+#  - all parameters that will be used during the search must be placed after the *.
+#  - the function must return a dict with at least a 'loss' key containing the result
+# of the loss function. You can add any additional metrics or information with other 
+# keys in the dict. See hyperopt documentation for more informations.
+def objective(dataset, config, *, iss, N, sr, lr, ridge, seed):
+    
+    # This step may vary depending on what you put inside 'dataset'
+    train_data, validation_data = dataset
+    X_train, y_train = train_data
+    X_val, y_val = validation_data
+    
+    # You can access anything you put in the config 
+    # file from the 'config' parameter.
+    instances = config["instances_per_trial"]
+    
+    # The seed should be changed across the instances, 
+    # to be sure there is no bias in the results 
+    # due to initialization.
+    variable_seed = seed 
+
+    losses = []; r2s = [];
+    for n in range(instances):
+        # Build your model given the input parameters
+        reservoir = Reservoir(N, 
+                              sr=sr, 
+                              lr=lr, 
+                              inut_scaling=iss, 
+                              seed=variable_seed)
+        
+        readout = Ridge(ridge=ridge)
+
+        model = reservoir >> readout
+
+
+        # Train your model and test your model.
+        predictions = model.fit(X_train, y_train) \
+                           .run(X_test)
+        
+        loss = nrmse(y_test, predictions, norm_value=np.ptp(X_train))
+        r2 = rsquare(y_test, predictions)
+        
+        # Change the seed between instances
+        variable_seed += 1
+        
+        losses.append(loss)
+        r2s.append(r2)
+
+    # Return a dictionnary of metrics. The 'loss' key is mandatory when
+    # using hyperopt.
+    return {'loss': np.mean(losses),
+            'r2': np.mean(r2s)}
+
+
+print("3. Define Research Space")
+
+hyperopt_config = {
+    "exp": f"hyperopt-multiscroll", # the experimentation name
+    "hp_max_evals": 200,             # the number of differents sets of parameters hyperopt has to try
+    "hp_method": "random",           # the method used by hyperopt to chose those sets (see below)
+    "seed": 42,                      # the random state seed, to ensure reproducibility
+    "instances_per_trial": 3,        # how many random ESN will be tried with each sets of parameters
+    "hp_space": {                    # what are the ranges of parameters explored
+        "N": ["choice", 500],             # the number of neurons is fixed to 500
+        "sr": ["loguniform", 1e-2, 10],   # the spectral radius is log-uniformly distributed between 1e-2 and 10
+        "lr": ["loguniform", 1e-3, 1],  # idem with the leaking rate, from 1e-3 to 1
+        "iss": ["choice", 0.9],           # the input scaling is fixed
+        "ridge": ["choice", 1e-7],        # and so is the regularization parameter.
+        "seed": ["choice", 1234]          # an other random seed for the ESN initialization
+    }
+}
+
+
+import json
+
+
+print("4. Prepare Data")
+# we precautionously save the configuration in a JSON file
+# each file will begin with a number corresponding to the current experimentation run number.
+with open(f"{hyperopt_config['exp']}.config.json", "w+") as f:
+    json.dump(hyperopt_config, f)
+
+train_len = 1000
+
+X_train = X[:train_len]
+y_train = X[1 : train_len + 1]
+
+X_test = X[train_len : -1]
+y_test = X[train_len + 1:]
+
+dataset = ((X_train, y_train), (X_test, y_test))
+
+print("5. Launch Hyperopt")
+from reservoirpy.hyper import research
+
+best = research(objective, dataset, f"{hyperopt_config['exp']}.config.json", ".")
+
+
+print("6. DIsplay Hyperparameters")
+from reservoirpy.hyper import plot_hyperopt_report
+
+fig = plot_hyperopt_report(hyperopt_config["exp"], ("lr", "sr"), metric="r2")
